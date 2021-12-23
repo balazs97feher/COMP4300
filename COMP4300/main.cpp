a395683c07228199a0f12b8a25223716a30c2cc7
Feher Balazs Csaba z0047p3d <balazs.feher@siemens.com>
2021-12-23 19:26:24 +0100

    Read input, generate shapes

--- COMP4300/main.cpp
+++ COMP4300/main.cpp
@@ -1,10 +1,156 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/System/Vector2.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+
+namespace fs = std::filesystem;
+
+class BouncingShape
+{
+public:
+    BouncingShape(std::unique_ptr<sf::Shape> &&shape, const sf::Vector2f velocity, const sf::Text& text, const sf::Vector2f &startingPos)
+        : mShape(std::move(shape)), mVelocity(velocity), mText(text)
+    {
+        mShape->setPosition(startingPos);
+        mText.setPosition(startingPos);
+    }
+
+    void update(sf::RenderWindow &window)
+    {
+        mText.move(mVelocity);
+        mShape->move(mVelocity);
+
+        window.draw(*mShape);
+        window.draw(mText);
+    }
+
+private:
+    std::unique_ptr<sf::Shape> mShape;
+    sf::Vector2f mVelocity;
+    sf::Text mText;
+};
+
+class ShapeFactory
+{
+public:
+    ShapeFactory(const sf::Font font, const sf::Color color) : mFont(font), mColor(color) {}
+
+    BouncingShape makeCircle(const std::string& text, const float radius, const sf::Color& color, const sf::Vector2f& velocity, const sf::Vector2f& startingPos)
+    {
+        sf::Text shapeText{ text, mFont };
+        shapeText.setFillColor(mColor);
+
+        std::unique_ptr<sf::Shape> shape = std::make_unique<sf::CircleShape>(radius);
+        shape->setFillColor(color);
+
+        return BouncingShape{ std::move(shape), velocity, shapeText, startingPos };
+    }
+
+    BouncingShape makeRectangle(const std::string& text, const sf::Vector2f& size, const sf::Color& color, const sf::Vector2f& velocity, const sf::Vector2f& startingPos)
+    {
+        sf::Text shapeText{ text, mFont };
+        shapeText.setFillColor(mColor);
+
+        std::unique_ptr<sf::Shape> shape = std::make_unique<sf::RectangleShape>(size);
+        shape->setFillColor(color);
+
+        return BouncingShape{ std::move(shape), velocity, shapeText, startingPos };
+    }
+
+private:
+    sf::Font mFont;
+    sf::Color mColor;
+};
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    const fs::path configFile{ "./assignment01_config.txt" };
+    if (!fs::exists(configFile))
+    {
+        std::cerr << "Configuration file " << configFile.string() << " does not exit." << std::endl;
+        exit(-1);
+    }
+
+    std::fstream configuration{ configFile, std::ios::in };
+    if (!configuration.is_open())
+    {
+        std::cerr << "Failed to open configuration file." << std::endl;
+        exit(-1);
+    }
+
+    std::string nextLine;
+    std::getline(configuration, nextLine);
+    
+    std::stringstream stream{ nextLine };
+
+    int wWidth, wHeight;
+    stream >> wWidth >> wHeight;
+
+    std::getline(configuration, nextLine);
+    stream = std::stringstream{ nextLine };
+
+    std::string fontName;
+    stream >> fontName;
+
+    sf::Font font;
+    if (!font.loadFromFile("./" + fontName))
+    {
+        std::cerr << "Failed to load font " << fontName << std::endl;
+        exit(-1);
+    }
+
+    int textSize;
+    stream >> textSize;
+
+    sf::Color textColor;
+    stream >> textColor.r >> textColor.g >> textColor.g;
+
+    std::vector<BouncingShape> shapes;
+    ShapeFactory shapeFactory{ font, textColor };
+
+    std::getline(configuration, nextLine);
+    while(!configuration.eof())
+    {
+        stream = std::stringstream{ nextLine };
+
+        std::string shape, text;
+        stream >> shape >> text;
+
+        sf::Vector2f startingPos, velocity;
+        stream >> startingPos.x >> startingPos.y >> velocity.x >> velocity.y;
+        int r, g, b;
+        stream >> r >> g >> b;
+        sf::Color shapeColor(r, g, b);
+
+        if (shape == "Circle")
+        {
+            float radius;
+            stream >> radius;
+
+            shapes.push_back(shapeFactory.makeCircle(text, radius, shapeColor, velocity, startingPos));
+        }
+        else
+        {
+            sf::Vector2f rectangleSize;
+            stream >> rectangleSize.x >> rectangleSize.y;
+
+            shapes.push_back(shapeFactory.makeRectangle(text, rectangleSize, shapeColor, velocity, startingPos));
+        }
+
+        std::getline(configuration, nextLine);
+    }
+
+
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Bouncing Shapes");
 
     while (window.isOpen())
     {
@@ -16,7 +162,12 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+
+        for (auto& shape : shapes)
+        {
+            shape.update(window);
+        }
+
         window.display();
     }
 
